4e813d0e362e4201bc0c87362540878bdd36c590
Xiongl09 <Xiongl09@mingyuanyun.com>
2021-04-27 17:55:29 +0800

    跳绳瑜伽

--- pages/skipping/index.js
+++ pages/skipping/index.js
@@ -1,40 +1,76 @@
-let count ;
+let count;
 Page({
   data: {
     nbFrontColor: '#000000',
     nbBackgroundColor: '#ffffff',
-    min:0,
-    seconds:0,
-    ms:0,
-    btnAbled:'true',
-    cal:0
+    min: 0,
+    seconds: 0,
+    ms: 0,
+    cal: 0,
+    START: true,
+    STOP: false,
+    END: false,
+    GROUP: false,
   },
   onLoad(options) {
-  //  this.start()
+    //  this.start()
   },
-  start(){
-    this.setData({btnAbled:false, min:0,
-      seconds:0,
-      ms:0,})
-    count= setInterval(()=>{
+  start() {
+    this.setData({
+   
+      STOP: true,
+      END: true,
+      START: false,
+    })
+    count = setInterval(() => {
       this.setData({
-        ms:this.data.ms+1
+        ms: this.data.ms + 1
       })
-      if(this.data.ms === 100){
-        console.log((1/360 * this.data.seconds).toFixed(2));
+      if (this.data.ms === 100) {
         this.setData({
-          ms:0,
-          seconds:this.data.seconds+1,
+          ms: 0,
+          seconds: this.data.seconds + 1,
         })
         this.setData({
-          cal:(1/360 * this.data.seconds).toFixed(2)
+          cal: (1 / 360 * this.data.seconds).toFixed(2)
         })
       }
-    },10)
+    }, 10)
   },
-  end(){
+  end() {
+    let that = this;
+    wx.showModal({
+      title: '提示',
+      content: '确认要结束运动吗，不再坚持一下下？',
+      success (res) {
+        if (res.confirm) {
+          clearInterval(count)
+          that.setData({
+            min: 0,
+            seconds: 0,
+            ms: 0,
+            cal:0,
+            STOP: false,
+            END: false,
+            START: true,
+          })
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+        }
+      }
+    })
+  },
+  stop() {
     clearInterval(count)
-    this.setData({btnAbled:true,
-     })
+    this.setData({
+      STOP: false,
+      END: true,
+      START: true,
+    })
+  },
+  show(showItems) {
+    this.setData({
+      showItems: [...this.data.showItems, ...showItems]
+    })
   }
 })
